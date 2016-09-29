35e275856f786a5fe7b7f3afb8d26f89dec5c920
SAE\ahockley2 <1001427@student.sae.edu.au>
2016-09-29 17:24:51 +1000

    first push

---
+++ colourPsyche/Assets/colourFul.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class colourFul : MonoBehaviour
+{
+    public Color[] colours;
+    public Button[] buttons;
+
+	// Use this for initialization
+	void Start ()
+    {
+        colours = new Color[7];
+        colours[0] = Color.red;
+        colours[1] = Color.magenta;
+        colours[2] = Color.blue;
+        colours[3] = Color.yellow;
+        colours[4] = Color.green;
+        colours[5] = Color.cyan;
+        colours[6] = Color.black;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    foreach
+	}
+}
